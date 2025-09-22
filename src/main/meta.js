761c3aa99e8f3ae937c65f692afe0779f66a3b04
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-09-22 11:44:44 -0400

    chore: add turbowarp staging, mistwarp and unsandboxed
    Signed-off-by: yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>

--- src/main/meta.js
+++ src/main/meta.js
@@ -16,6 +16,7 @@
 // @match       https://www.scratch-cn.cn/*
 // @match       https://www.40code.com/*
 // @match       https://turbowarp.org/*
+// @match       https://staging.turbowarp.org/*
 // @match       https://codingclip.com/*
 // @match       https://editor.turbowarp.cn/*
 // @match       https://0832.ink/rc/*
@@ -26,6 +27,8 @@
 // @match       https://code.xueersi.com/*
 // @match       https://electramod.vercel.app/*
 // @match       https://xplab.vercel.app/*
+// @match       https://warp.mistium.com/*
+// @match       https://alpha.unsandboxed.org/*
 // @run-at      document-start
 // @grant       none
 // @version     process.env.VERSION
