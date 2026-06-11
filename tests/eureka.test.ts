cf1a1a51cc11928004d3329b29027fafdb86df45
Simon Shiki <sinangentoo@gmail.com>
2026-06-11 10:16:52 +0000

    :wrench: chore: don't test clipcc stable version

--- tests/eureka.test.ts
+++ tests/eureka.test.ts
@@ -11,7 +11,7 @@ const TEST_SITES = [
   'https://gonfunko.github.io/scratch-gui/',
   'https://world.xiaomawang.com/scratch3-playground?platform=3',
   'https://code.xueersi.com/scratch/index.html?pid=2&version=3.0&preview=preview&env=community',
-  'https://codingclip.com/editor/stable/',
+  'https://codingclip.com/editor/',
   'https://cocrea.world/gandi'
 ];
 
